31d8a7ba98e20cf6ed8415de9d3b52a0b2f33f7c
msmi1023 <msmi1023@ford.com>
2016-07-25 10:42:57 -0400

    updated tests for app delegate

--- AdventureBuilderTests/AppDelegateSpec.mm
+++ AdventureBuilderTests/AppDelegateSpec.mm
@@ -1,5 +1,6 @@
 #import <Cedar/Cedar.h>
 #import "AppDelegate.h"
+#import "ListBookingViewController.h"
 
 using namespace Cedar::Matchers;
 using namespace Cedar::Doubles;
@@ -16,6 +17,32 @@ describe(@"AppDelegate", ^{
 	
 	it(@"should exist", ^{
 		subject should_not be_nil;
+	});
+	
+	it(@"should respond to the didFinishLaunchingWithOptions selector", ^{
+		[subject respondsToSelector:@selector(application:didFinishLaunchingWithOptions:)] should be_truthy;
+		
+	});
+	
+	describe(@"didFinishLaunchingWithOptions", ^{
+		beforeEach(^{
+			//ignore the nullable warning here, doesn't matter for the purposes of our test
+			[subject application:nil didFinishLaunchingWithOptions:nil];
+		});
+		
+		it(@"should create a window object and store it on the delegate's property", ^{
+			[subject.window isKindOfClass:[UIWindow class]] should be_truthy;
+		});
+		
+		it(@"should have loaded the root view controller into the window object", ^{
+			[subject.window.rootViewController isKindOfClass:[UINavigationController class]] should be_truthy;
+			
+			[subject.window.rootViewController.childViewControllers.firstObject isKindOfClass:[ListBookingViewController class]] should be_truthy;
+		});
+		
+		it(@"should have injected the appropriate dependencies into the initial view controller", ^{
+			[((ListBookingViewController *)subject.window.rootViewController.childViewControllers.firstObject).bookingService isKindOfClass:[BookingService class]] should be_truthy;
+		});
 	});
 });
 
